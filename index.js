6ee59103e865778df182d364c401ebf56371040e
jajimajp <jajima.jp@gmail.com>
2022-12-19 21:44:12 +0900

    Update index.js
    Implement function to change directory

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const path = require('path')
 const { lookpath } = require('lookpath')
 const { spawn } = require('node:child_process')
 const log = console.log
+
+
+const loop = () => {
+  let nextPath = ''
+  const cwd = process.cwd()
+  const child = spawn('peco')
+  child.stdout.on('data', d => nextPath = d.toString().trim())
+  child.on('exit', () => {
+    if (nextPath === '') {
+      log('exit')
+      process.exit(0)
+    } else {
+      try {
+        process.chdir(path.join(cwd, nextPath))
+      } catch (err) {
+        process.exit(1)
+      }
+      loop()
+    }
+  })
+  const files = fs.readdirSync(cwd)
+  files.forEach(i => child.stdin.write(`${i}\n`))
+}
 
 const main = async () => {
   const peco = await lookpath('peco')
@@ -10,11 +34,11 @@ const main = async () => {
     log('peco does not found. Check if peco is installed and added to $PATH.')
     process.exit(1)
   }
-  const cwd = process.cwd()
-  const child = spawn('peco')
-  child.stdout.on('data', d => log('[peco] ', d.toString()))
-  const files = fs.readdirSync(cwd)
-  files.forEach(i => child.stdin.write(`${i}\n`))
+  process.on('SIGINT', () => {
+    log('SIGINT is passed. exit.')
+    process.exit(1)
+  })
+  loop()
 }
 
 main()
