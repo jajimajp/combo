29ec8b88d1e0e9b6b3b80d7f83ce09ce45ca6682
jajimajp <jajima.jp@gmail.com>
2022-12-19 20:57:33 +0900

    Update index.js
    Check if peco is installed

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
-(async () => {
+const log = console.log
+
+const main = async () => {
   const { lookpath } = require('lookpath')
-  console.log('Combo version 1.0.0')
+  log('Combo version 1.0.0')
 
   const peco = await lookpath('peco')
-  console.log(peco)
+  if (peco === undefined) {
+    log('peco does not found. Check if peco is installed and added to $PATH.')
+    process.exit(1)
+  } else {
+    log('use peco at', peco)
+  }
+}
 
-  const TCTDNE = await lookpath('TheCommandThatDoesNotExist')
-  console.log(TCTDNE)
-})()
+main()
