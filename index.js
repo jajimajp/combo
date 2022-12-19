91a06a1cbfa9ca1f9446d1f5de2f293c63598a12
jajimajp <jajima.jp@gmail.com>
2022-12-19 21:20:02 +0900

    Update index.js
    Input filenames to peco

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const fs = require('fs')
 const { lookpath } = require('lookpath')
 const { spawn } = require('node:child_process')
 const log = console.log
@@ -9,11 +9,12 @@ const main = async () => {
   if (peco === undefined) {
     log('peco does not found. Check if peco is installed and added to $PATH.')
     process.exit(1)
-  } else {
-    log('use peco at', peco)
   }
-  const child = spawn('peco', ['--version'])
-  child.stdout.on('data', d => log(d.toString()))
+  const cwd = process.cwd()
+  const child = spawn('peco')
+  child.stdout.on('data', d => log('[peco] ', d.toString()))
+  const files = fs.readdirSync(cwd)
+  files.forEach(i => child.stdin.write(`${i}\n`))
 }
 
 main()
